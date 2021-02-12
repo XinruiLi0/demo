d285df5e39eb8f215d63555fbbb2bff02851659e
XinruiLi0 <xinruili3@cmail.carleton.ca>
2021-02-12 10:39:18 -0500

    Add unit test

---
+++ src/test/java/lab/AddressBookTest.java
@@ -0,0 +1,44 @@
+package lab;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AddressBookTest {
+    private AddressBook aBook;
+    private BuddyInfo b1;
+    private BuddyInfo b2;
+
+    @Before
+    public void setUp() throws Exception {
+        this.aBook = new AddressBook();
+        this.b1 = new BuddyInfo("Mike", 1234567890, 19);
+        this.b2 = new BuddyInfo("John", 1111111111, 16);
+        this.aBook.addBuddy(this.b1);
+    }
+
+    @Test
+    public void size() {
+        assertEquals("AddressBook should have a size 1.", 1, aBook.size());
+    }
+
+    @Test
+    public void addBuddy() {
+        this.aBook.addBuddy(this.b2);
+        assertEquals("AddressBook should have a size 2.", 2, aBook.size());
+    }
+
+    @Test
+    public void removeBuddy() {
+        this.aBook.removeBuddy(this.b1);
+        assertEquals("AddressBook should have a size 0.", 0, aBook.size());
+    }
+
+    @Test
+    public void clear() {
+        this.aBook.clear();
+        assertEquals("AddressBook should have a size 0.", 0, aBook.size());
+    }
+}
---
+++ src/test/java/lab/BuddyInfoTest.java
@@ -0,0 +1,54 @@
+package lab;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BuddyInfoTest {
+
+    private BuddyInfo b1;
+    private BuddyInfo b2;
+
+    @Before
+    public void setUp()
+            throws Exception
+    {
+        this.b1 = new BuddyInfo("Mike", 1234567890L, 19);
+        this.b2 = new BuddyInfo("John", 1111111111L, 16);
+    }
+
+    @Test
+    public void getAge() {
+        assertEquals("Mike age should be 19.", 19, b1.getAge());
+    }
+
+    @Test
+    public void setAge() {
+        b1.setAge(20);
+        assertEquals("Mike age should be 20.", 20, b1.getAge());
+    }
+
+    @Test
+    public void getName() {
+        assertEquals("The name should be Mike.", "Mike", b1.getName());
+    }
+
+    @Test
+    public void setName() {
+        b1.setName("Jason");
+        assertEquals("The name should be Jason.", "Jason", b1.getName());
+    }
+
+    @Test
+    public void getPhoneNumber() {
+        assertEquals("The phone number should be 1234567890L.", 1234567890L, b1.getPhoneNumber());
+    }
+
+    @Test
+    public void setPhoneNumber() {
+        b1.setPhoneNumber(1111111111L);
+        Assert.assertEquals("The phone number should be 1111111111L.", 1111111111L, b1.getPhoneNumber());
+    }
+}
